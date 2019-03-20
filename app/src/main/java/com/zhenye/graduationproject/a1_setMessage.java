3603af8502a800c09526cec8cbf4b94a0ad2d6fb
ambe <13543479929@163.com>
2019-03-20 17:16:25 +0800

    4.创建a1_setMessage,添加输入功能

--- app/src/main/java/com/zhenye/graduationproject/a1_setMessage.java
+++ app/src/main/java/com/zhenye/graduationproject/a1_setMessage.java
@@ -2,13 +2,26 @@ package com.zhenye.graduationproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class a1_setMessage extends AppCompatActivity {
-
+    public String recMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a1_set_message);
+        final EditText  setMessage = findViewById(R.id.editText2_setMessage);
+        Button setButton = findViewById(R.id.button2_setMessage);
+        final TextView showMessage = findViewById(R.id.showMessage);
+        setButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               showMessage.setText( showMessage.getText()+ setMessage.getText().toString());
 
+            }
+        });
     }
 }
